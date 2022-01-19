c9f97456e7d79f7e7a1f1c8683ddeb644d48583f
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-01-19 15:55:21 +0100

    Add folder and file for testing, and seperate creation of app and server listening

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-import express from "express";
-import { engine } from "express-handlebars";
-
-const app = express();
-
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./views");
-
-app.get("/", (req, res) => {
-  res.render("home");
-  console.log(req.path);
-});
-
-app.use(express.static("./"));
+import app from "./src/js/app.js";
 
 app.listen(5080);
---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { engine } from "express-handlebars";
+
+const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.render("home");
+  console.log(req.path);
+});
+
+app.use(express.static("./"));
+
+export default app;
