6de73ba5048cbd1173354733624d0625476469a7
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-01-19 18:39:16 +0100

    Add test to see that the title matches with the id

--- test/pages.spec.js
+++ test/pages.spec.js
@@ -4,3 +4,9 @@ import request from "supertest";
 test("show landing page when requested", async () => {
   const response = await request(app).get("/").expect(200);
 });
+
+test("show right name for movie on that movies page", async () => {
+  const response = await request(app).get("/2").expect(200);
+
+  expect(response.text.includes("Godfather")).toBeTruthy();
+});
