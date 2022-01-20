0dd8bec22ae292c8f23716db1d77abda73f1ef3b
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-01-20 08:39:48 +0100

    Refactor

--- src/js/modules/fetchData.js
+++ src/js/modules/fetchData.js
@@ -1,15 +1,17 @@
 import fetch from "node-fetch";
 
 export async function fetchMovies() {
-  const url = "https://lernia-kino-cms.herokuapp.com/api/movies";
-  const response = await fetch(url);
+  const response = await fetch(
+    "https://lernia-kino-cms.herokuapp.com/api/movies"
+  );
   const data = await response.json();
   return data.data;
 }
 
 export async function fetchMovie(id) {
-  const url = `https://lernia-kino-cms.herokuapp.com/api/movies/${id}`;
-  const response = await fetch(url);
+  const response = await fetch(
+    `https://lernia-kino-cms.herokuapp.com/api/movies/${id}`
+  );
   const data = await response.json();
   return data.data;
 }
