5584059c7e0636412c3e4bbfe1ef11e9eb162a51
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-01-20 08:43:54 +0100

    Add test for movies page

--- test/pages.spec.js
+++ test/pages.spec.js
@@ -5,6 +5,12 @@ test("show landing page when requested", async () => {
   const response = await request(app).get("/").expect(200);
 });
 
+test("show movies page with movies when requested", async () => {
+  const response = await request(app).get("/movies").expect(200);
+
+  expect(response.text.includes("Idiocracy")).toBeTruthy();
+});
+
 test("show right name for movie on that movies page", async () => {
   const response = await request(app).get("/2").expect(200);
 
